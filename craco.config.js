d5afc1f03bc0c3b318d922baa9d86fa8925e1ff2
Silvio <silvio.dias@etec.sp.gov.br>
2021-11-30 16:01:07 -0300

    #refactor - Changing the craco configuration

--- craco.config.js
+++ craco.config.js
@@ -1,14 +1,15 @@
+/* eslint-disable no-undef */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const CracoAlias = require('craco-alias')
 
+// eslint-disable-next-line no-undef
 module.exports = {
-  plugins: [
-    {
-      plugin: CracoAlias,
-      options: {
-        source: 'tsconfig',
-        baseUrl: './src',
-        tsConfigPath: './tsconfig.paths.json'
-      }
+  plugins: [{
+    plugin: CracoAlias,
+    options: {
+      source: 'tsconfig',
+      baseUrl: './src',
+      tsConfigPath: './tsconfig.paths.json'
     }
-  ]
+  }]
 }
