a92beda0a42e738fa542ff092db098b522876da3
Silvio <silvio.dias@etec.sp.gov.br>
2021-11-30 17:14:48 -0300

    #feature - add storageProvider and themeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
+import { ThemeProvider } from 'styled-components'
+
+import { StorageProvider } from './contexts/StorageContext'
+
+import { Home } from './pages/Home'
 
 import { GlobalStyle } from './global/styles/global'
+import theme from './global/styles/theme'
 
 export const App: React.FC = () => {
   return (
-    <>
-      <h1>Todo</h1>
+    <ThemeProvider theme={theme}>
+      <StorageProvider>
+        <Home />
+      </StorageProvider>
       <GlobalStyle />
-    </>
+    </ThemeProvider>
   )
 }
