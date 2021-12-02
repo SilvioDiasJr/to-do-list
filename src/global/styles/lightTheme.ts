a2e8a10992ac40927734c6e56d0467d621155eab
Silvio <silvio.dias@etec.sp.gov.br>
2021-12-02 12:56:39 -0300

    #feature - Creating light theme and black theme

---
+++ src/global/styles/lightTheme.ts
@@ -0,0 +1,20 @@
+export default {
+  colors: {
+    title: '#181824',
+    text: '#25273C',
+    placeholder: '#c7c9c7',
+
+    filter_active: '#03a9fc',
+
+    input_background: '#ffffff',
+
+    card_background: '#ffffff',
+    circle: '#cccfcc',
+
+    background: '#fAfAfA',
+  },
+  fonts: {
+    regular: 400,
+    bold: 700
+  }
+}
