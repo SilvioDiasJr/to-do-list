afbb57900b84b497696e78993e611f84607a5977
Silvio <silvio.dias@etec.sp.gov.br>
2021-12-05 01:07:57 -0300

    #refactor - changing  colors darkTheme

--- src/global/styles/darkTheme.ts
+++ src/global/styles/darkTheme.ts
@@ -2,18 +2,18 @@ export default {
   colors: {
     title: '#ffffff',
     text: '#e1e3e1',
-    placeholder: '#465766',
+    placeholder: '#556a7d',
 
     filter_active: '#03a9fc',
 
     input_background: '#25273C',
 
     card_background: '#25273C',
-    circle: '#465766',
+    circle: '#556a7d',
 
     title_button_confirm: '#ffffff',
 
-    background_overlay_modal:'#ffffff80',
+    background_overlay_modal:'#ffffff90',
 
     background: '#181824',
   },
